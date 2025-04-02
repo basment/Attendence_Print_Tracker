810b987745b903c0166cb7c3b14ba9ed4d41d4c9
Phat Dang <dangtanphat2003@gmail.com>
2025-04-02 18:53:48 -0400

    T

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 from database import get_connection
 from functions.pull_events import pull_events
+from functions.registered_users import registered_users 
 
 app = Flask(__name__)
 CORS(app)
@@ -15,6 +16,9 @@ def home():
 def handle_pull_events():
     return pull_events()
 
+@app.route("/api/registered_users", meothods=["GET"])
+def registered_users():
+    return registered_users()
 
 if __name__ == "__main__":
     app.run(debug=True)
